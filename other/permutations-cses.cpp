0237a585410e2ccbcac3d94a6c186f8be5285bea
Lucas Jin <kblazer20@gmail.com>
2024-04-04 22:18:08 -0400

    Finished permutations-cses.cpp

---
+++ other/permutations-cses.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<long long int> VLL;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int l;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int  ull;
+typedef long double ld;
+
+
+int main() {
+    int n;
+    cin >> n;
+    VI ord;
+    if (n>3 and n!=1) {
+        FOR(i, 2, n+1, 2) {
+            ord.PB(i);
+        }
+        FOR(i, 1, n+1, 2) {
+            ord.PB(i);
+        }
+        for(auto i: ord) {
+            cout << i << " ";
+        }
+    } else if (n==1) {
+        cout << "1";
+    } else {
+        cout << "NO SOLUTION";
+    }
+    return 0;
+}
