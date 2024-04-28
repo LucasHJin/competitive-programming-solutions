2bb7f24f9e9006a8a57031d80a82b6b4925fd768
Lucas Jin <kblazer20@gmail.com>
2024-04-28 15:15:56 -0400

    Finished movie festival - cses

---
+++ search_sort/movie_festival-cses.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+#include <map>
+ 
+typedef long long ll;
+ 
+using namespace std;
+
+
+bool sec(const pair<int, int> &a, const pair<int, int> &b) {
+    return a.second < b.second;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int count = 0;
+    int end = 0;
+
+    vector<pair<int, int>> movies(n);
+
+    for (int i=0; i<n; i++) {
+        cin >> movies[i].first >> movies[i].second;
+    }
+    sort(movies.begin(), movies.end(), sec);
+
+    for (int i=0; i<n; i++) {
+        if (movies[i].first >= end) {
+            ++count;
+            end = movies[i].second;
+        }
+    }
+    
+    cout << count << endl;
+
+    return 0;
+    
+}
