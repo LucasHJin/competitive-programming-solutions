fb61b483a3b1cbe36a8fb96c54b91e50b2f9fbee
Lucas Jin <kblazer20@gmail.com>
2024-07-11 21:46:50 -0400

    Did BIT dynamic range sum queries - cses

---
+++ data_structures/binary_indexed_tree/BIT_dynamic_range_sum_queries-cses.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <iostream>
+ 
+typedef long long ll;
+const int maxN = 2e5;
+ 
+using namespace std;
+
+//O(logn)
+ll pre_sum(int idx, ll bit[]) {
+    ll sum = 0;
+    while (idx>0) {
+        sum += bit[idx];
+        //bit expression go to next good (1, 2, 4...)
+        idx -= (idx & (-idx));
+    }
+    return sum;
+}
+
+//O(logn)
+void add(int idx, int n, ll bit[], int val) {
+    while (idx<=n) {
+        bit[idx] += val;
+        //bit expression go to next good (1, 2, 4...)
+        idx += (idx & (-idx));
+    }
+}
+
+//O(logn)
+void update(int idx, int n, ll bit[], int val, vector<ll> x) {
+    ll temp = val - x[idx];
+    while (idx<=n) {
+        bit[idx] += temp;
+        idx += (idx & (-idx));
+    }
+    x[idx] = val;
+}
+
+int main() {
+    cin.sync_with_stdio(0); cin.tie(0);
+    int n, q;
+    cin >> n >> q;
+    vector<ll> x(n+1);
+    for (int i=1; i<=n; i++) {
+        cin >> x[i];
+    }
+    ll bit[maxN+1];
+    for (int i=0; i<=n; i++) {
+        bit[i] = 0;
+    }
+    
+    for (int i=1; i<=n; i++) {
+        add(i, n, bit, x[i]);
+    }
+
+    int a, b, c;
+    for (int i=0; i<q; i++) {
+        cin >> a >> b >> c;
+        if (a==1) {
+            update(b, n, bit, c, x);
+        } else {
+            if (b==1) {
+                cout << pre_sum(c, bit) << endl;
+            } else {
+                cout << pre_sum(c, bit) - pre_sum(b-1, bit) << endl;
+            }
+        }
+    }
+    
+    return 0;
+}
+
