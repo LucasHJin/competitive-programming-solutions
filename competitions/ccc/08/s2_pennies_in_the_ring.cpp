c31d5db5f39ad0b6c0ced1d1ae75ffd30e53dd48
Lucas Jin <kblazer20@gmail.com>
2024-10-30 05:29:49 -0400

    Finished pennies in the ring - dmoj

--- competitions/ccc/08/s2_pennies_in_the_ring.cpp
+++ competitions/ccc/08/s2_pennies_in_the_ring.cpp
@@ -10,21 +10,15 @@ int main() {
     int x;
     cin >> x;
     while (x!=0) {
-        int c = 8;
-        int ans = 5;
-        bool yes = true;
-        while (x>1) {
-            ans+=c;
-            if (yes) {
-                c+=8;
-                yes=false;
-            } else {
-                c+=4;
-                yes=true;
-            }
-            x--;
+        if (x==0) 
+            break;
+        int cnt = 0;
+        //calculate max y value point for each x value and add that (so that it has all those coords)
+        for (int i=0; i<=x; i++) {
+            cnt += static_cast<int>(sqrt(x*x-i*i));
         }
-        cout << ans << endl;
+        //#4 for symmetry, +1 for (0, 0)
+        cout << cnt*4+1 << endl;
 
         cin >> x;
     }
